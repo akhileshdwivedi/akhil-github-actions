96fe9747ad66539d95be133aae45042acd0709a8
akhileshdwivedi <akhileshdwivedi25@gmail.com>
2025-07-24 15:47:09 +0100

    update slack key

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const API_KEY = "a-dummy-password";
 
 const slack =
-	"https://hooks.slack.com/services/HGHJGHJGJHG/KJHJKHJKHJK/JHGHJJHUIY78YJHUIJHUI";
+	"https://hooks.slack.com/services/HGHJGHJGJHG/KJHJKHJKHJK/sjhfusajsfdfiso79fs8d0f9skdfjksdjfkskjf";
