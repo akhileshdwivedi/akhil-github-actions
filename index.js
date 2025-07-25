9e6eb8f0316ac5cbb250f207709fcccb50958644
akhileshdwivedi <akhileshdwivedi25@gmail.com>
2025-07-25 15:49:41 +0100

    add slack key

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 const API_KEY = "a-dummy-password";
 const GITHUB_TOKEN = "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";
+const slack_incoming_webhook_url = 'https://hooks.slack.com/services/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'
